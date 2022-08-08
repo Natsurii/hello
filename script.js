5052ec50a7d8af9d07b1131d87e5b4aefd3ff00f
Natsurii <unatsurii@gmail.com>
2022-08-08 16:26:42 +0800

    add changes to the script and css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DELAY_IN_MS = 2500;
 const wordHighlight = document.querySelector('#highlight');
-let wordList = ['create', 'build', 'design', 'explore', 'develop', 'assemble', 'compile', 'compose', 'fabricate', 'synthesize', 'jerry-build', 'start', 'make', 'plan', 'formulate', 'colab', 'coop', 'conspire', 'collude', 'brainstorm', 'analyze', 'fix', 'conceptualize']
+let wordList = ['create', 'build', 'design', 'start', 'make', 'plan', 'colab', 'coop', 'fix']
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,8 +9,10 @@ function delay(ms) {
 (async()=>{
     for (let index = 0; index < Infinity; index++) {
      let randomWord = wordList[Math.floor(Math.random() * wordList.length)]
-    await delay(DELAY_IN_MS);
-    wordHighlight.textContent = randomWord;
+     wordHighlight.textContent = randomWord;
+     await delay(DELAY_IN_MS);
+    
+
     } 
   })()
 
