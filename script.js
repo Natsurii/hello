5fb6145cd3d34c57ea2480c02872568addc25d00
Natsurii <unatsurii@gmail.com>
2022-08-10 15:59:57 +0800

    lessen global scope variables
    Changes:
    - Document selector wordHiglight and wordList moved inside the async
    IIFE for less scope pollution.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const DELAY_IN_MS = 2500;
-const wordHighlight = document.querySelector('#highlight');
-let wordList = ['create', 'build', 'design', 'start', 'make', 'plan', 'colab', 'coop', 'fix']
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 (async()=>{
+  const wordHighlight = document.querySelector('#highlight');
+  let wordList = ['create', 'build', 'design', 'start', 'make', 'plan', 'colab', 'coop', 'fix']
+  
     for (let index = 0; index < Infinity; index++) {
      let randomWord = wordList[Math.floor(Math.random() * wordList.length)]
      wordHighlight.textContent = randomWord;
